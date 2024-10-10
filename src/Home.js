dd0d0594549bba78f091d9af90c9fc10351380f8
Kapuriya Naimish Dineshbhai <naimish@spheretech.dev>
2024-10-10 17:40:08 +0530

    set-banner-video

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,21 @@
 import React from "react";
-import BannerVideo from "../src/images/banner-video.mp4";
+import BannerVideo from "../src/images/banner-video-1.mp4";
 
 const Home = () => {
   return (
-    <div className="main_container bg_black py-60px">
-      <div className="row">
-        <div className="col-6">
-          <h1 className="mb-0 text-white font_family_cromorant text_font_85px">
-            Hello! I’m <br />
-            <span className="light_black_color">Naimish patel</span>
-          </h1>
-        </div>
-        <div className="col-6">
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="w-100"
-          >
-            <source src={BannerVideo} type="video/mp4" />
-          </video>
-        </div>
-      </div>
+    <div className="video_container w-100 overflow-hidden position-relative">
+      <div className="gradient_overlay position-absolute top-0 start-0 w-100 h-100"></div>
+      <video autoPlay loop muted playsInline className="banner_video position-absolute top-50 start-50 w-100 h-100 translate-middle object-fit-cover">
+        <source src={BannerVideo} type="video/mp4" />
+      </video>
+      <h1 className="mb-0 text-white font_family_cromorant text_font_85px position-absolute translate-middle top-50 start-50 banner_title">
+        Hello! I’m <br />
+        <span className="light_black_color">Naimish patel</span>
+      </h1>
     </div>
   );
 };
 
 export default Home;
+
+
