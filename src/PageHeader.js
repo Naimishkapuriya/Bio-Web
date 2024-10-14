a04340a02888f4ca340053fd4772f05b3fcd7fec
Kapuriya Naimish Dineshbhai <naimish@spheretech.dev>
2024-10-14 15:37:02 +0530

    hire me icon add

--- src/PageHeader.js
+++ src/PageHeader.js
@@ -9,7 +9,7 @@ const PageHeader = () => {
     <header className="bg_header_color fix_header position-fixed top-0 w-100">
       <div className="main_container d-flex justify-content-between align-items-center">
         <Link data-aos="zoom-out">
-          <img  src={`${MainLogo}`} width={100} height={100} alt="main-logo" />
+          <img src={`${MainLogo}`} width={100} height={100} alt="main-logo" />
         </Link>
         <ul className="d-flex header_list mb-0 p-0">
           {menuItems.map((item, index) => (
@@ -28,8 +28,11 @@ const PageHeader = () => {
             </li>
           ))}
         </ul>
-        <button data-aos="zoom-out" className="header_btn text_font_22px font_family_cromorant font_weight_600">
-          Hire me
+        <button
+          data-aos="zoom-out"
+          className="header_btn text_font_22px font_family_cromorant font_weight_600"
+        >
+          Hire me <i class="fa fa-angle-double-right icon_font_16px" aria-hidden="true"></i>
         </button>
       </div>
     </header>
